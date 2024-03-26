2c43fe954113fec779ca968ecb95bd71f52b74d6
Perchik <ilya1880@mail.ru>
2024-03-26 14:05:00 +0300

    Класс Person.

--- What_is_class.py
+++ What_is_class.py
@@ -1,24 +1,21 @@
-"""Замена словаря"""
+"""Пишем класс Person"""
 
+# class Person:
+#     def __init__(self, name, job, pay):  # Конструктор принимает три аргумента
+#         self.name = name  # self - это созданный в будущем экз.класса. name - это информ. о состоянии
+#         self.job = job # job - это переменная из вне, которую мы сохраняем в классе как self.job,что бы использовать позже.
+#         self.pay = pay
+class Person:
+    """Персонаж может не иметь работы и соответственно зарплаты"""
+    def __init__(self, name, job=None, pay=0):
+        if isinstance(name,str):
+            self.name = name
+        else:
+            raise "Ошибка ввода"
+        self.job = job
+        self.pay = pay
+    def __str__(self):
+        return f'Имя: {self.name}\nДолжность: {self.job}\nЗП: {self.pay}'
 
-class Simple: pass
-
-
-
-if __name__ == '__main__':
-    persona_1 = Simple()
-    persona_1.name = 'Илья'
-    persona_1.age = '43'
-    persona_1.jobs = 'Busdriver'
-
-    persona_2 = Simple()
-    persona_2.name = 'Вика'
-    persona_2.age = '41'
-    persona_2.jobs = 'Teacher'
-
-    print(persona_1.jobs)
-    print(persona_2.jobs)
-
-
-
-
+P = Person('Илья','Bus',1500)
+print(P)
