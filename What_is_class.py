514d2b38ce5e8ca2dfc4fceff00d133550c9b23d
Perchik <ilya1880@mail.ru>
2024-03-26 10:29:30 +0300

    Замена словаря.

--- What_is_class.py
+++ What_is_class.py
@@ -1,31 +1,16 @@
-"""Простеший класс и что внутри
-   атрибут __dict__,__class__"""
+"""Замена словаря"""
 
 
 class Simple: pass
 
+dct = {}
 
 if __name__ == '__main__':
-    """Создание атрибутов класса виртуально без экз.класса"""
-    Simple.name = 'Vika'
-    Simple.age = 41
-    print(Simple.name, Simple.age)
-    """Создание экз.класса"""
-    X = Simple()
-    Y = Simple()
+    dct['name'] = 'Илья'
+    dct['age'] = '43'
+    dct['jobs'] = 'Busdriver'
 
-    print(X.name)  # Появляются атрибуты взятые из класса
-    print(Y.name)  # Появляются атрибуты взятые из класса
-
-    X.name = 'Илья'  # X Получает собственный атрибут
-
-    print(X.name)
-    print(Y.name)
-
-    """Aтрибут __dict__"""
-    print(list(Simple.__dict__.keys())) # ['__module__', '__dict__', '__weakref__', '__doc__', 'name', 'age']
-    print(list(X.__dict__.keys())) # Х- Собственный атрибут name
-    print(list(Y.__dict__.keys())) # Y- Отсутствуют атрибуты
-    print(X.__dict__['name']) # Обращение к атрибуту через словарь
-    print(X.__class__) # Показывает связь с классом
+    Simple.name = 'Илья'
+    Simple.age = '43'
+    Simple.jobs = 'Busdriver'
 
