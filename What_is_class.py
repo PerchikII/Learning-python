a751f1968a3db049676d8537e8d41ed0dccf2ed5
Perchik <ilya1880@mail.ru>
2024-03-26 10:38:15 +0300

    Работа с классом persona_1 persona_2

--- What_is_class.py
+++ What_is_class.py
@@ -3,14 +3,22 @@
 
 class Simple: pass
 
-dct = {}
+
 
 if __name__ == '__main__':
-    dct['name'] = 'Илья'
-    dct['age'] = '43'
-    dct['jobs'] = 'Busdriver'
+    persona_1 = Simple()
+    persona_1.name = 'Илья'
+    persona_1.age = '43'
+    persona_1.jobs = 'Busdriver'
 
-    Simple.name = 'Илья'
-    Simple.age = '43'
-    Simple.jobs = 'Busdriver'
+    persona_2 = Simple()
+    persona_2.name = 'Вика'
+    persona_2.age = '41'
+    persona_2.jobs = 'Teacher'
+
+    print(persona_1.jobs)
+    print(persona_2.jobs)
+
+
+
 
