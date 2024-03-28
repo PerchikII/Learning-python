23be332b7d645b1727fba28c2626b4b06caa0072
Perchik <ilya1880@mail.ru>
2024-03-28 08:32:32 +0300

    Добавление методов, реализующих поведение.

--- What_is_class.py
+++ What_is_class.py
@@ -1,21 +1,26 @@
-"""Пишем класс Person"""
+"""Добавление методов, реализующих поведение"""
 
-# class Person:
-#     def __init__(self, name, job, pay):  # Конструктор принимает три аргумента
-#         self.name = name  # self - это созданный в будущем экз.класса. name - это информ. о состоянии
-#         self.job = job # job - это переменная из вне, которую мы сохраняем в классе как self.job,что бы использовать позже.
-#         self.pay = pay
+
 class Person:
     """Персонаж может не иметь работы и соответственно зарплаты"""
-    def __init__(self, name, job=None, pay=0):
-        if isinstance(name,str):
-            self.name = name
-        else:
-            raise "Ошибка ввода"
+
+    def __init__(self, name: str, job: str = None, pay: int = 0):  # Конструктор принимает три аргумента
+        self.name = name
         self.job = job
         self.pay = pay
-    def __str__(self):
-        return f'Имя: {self.name}\nДолжность: {self.job}\nЗП: {self.pay}'
 
-P = Person('Илья','Bus',1500)
-print(P)
+    def give_last_name(self):
+        return self.name.split()[-1]
+
+    def give_rise(self, percent):
+        return int(((self.pay / 100)*percent) + self.pay)
+
+
+    def __repr__(self):
+        return f'Имя: {self.name.split()[-1]}\nДолжность: {self.job}\nЗП: {self.pay}'
+
+
+ilya = Person('Илья', 'Bus', 2000)
+vika = Person('Вика Алесина', 'Booh', 1500)
+print(ilya.give_last_name(), ilya.give_rise(50))
+print(vika.give_last_name(), vika.give_rise(20))
