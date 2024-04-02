e014568b1bfafd65742301dbbf553a7abf9504ac
Perchik <ilya1880@mail.ru>
2024-04-02 11:42:29 +0300

    Вложенные классы: правила областей видимости LEGB

--- What_is_class.py
+++ What_is_class.py
@@ -1,42 +1,19 @@
-"""Методики связывания классов
-Абстрактный суперкласс. NotImplementedError"""
+"""Вложенные классы:
+правила областей видимости LEGB"""
 
-class Super:
-    def method(self):
-        print('in Super.method')
-
-    def delegate(self):
-        self.action()
-    def action(self):
-        raise NotImplementedError ("Метод 'action' Не реализован")
-
-
-
-class Inheritor(Super):
-    pass
-
-
-class Replacer(Super):
-    def method(self):
-        print('in Replacer.method')
-
-
-class Extender(Super):
-    def method(self):
-        print('starting Extender.method')
-        Super.method(self)
-        print('ending Extender.method')
-
-
-class Provider(Super):...
-    # def action(self):
-    #     print('in Provider.action')
-
-
-if __name__ == '__main__':
-    for klass in (Inheritor, Replacer, Extender):
-        print('\n' + klass.__name__)
-        klass().method()
-    print('\nProvider...')
-    x = Provider()
-    x.delegate()
+X = 1
+def nester():
+    print(X,'ф-ция nester')
+    class C:
+        print(X,'класс С')
+        def method_1(self):
+            print(X,'method_1')
+        def method_2(self):
+            X = 3
+            print(X,'method_2')
+    I = C()
+    I.method_1()
+    I.method_2()
+print(X,"Глобальный")
+nester()
+print('-' * 40)
