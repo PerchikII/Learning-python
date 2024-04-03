49e34fc8dc7d9e7cae8775075cb65f9b3c6a1480
Perchik <ilya1880@mail.ru>
2024-04-03 14:02:12 +0300

    Правила областей видимости LEGB.

--- What_is_class.py
+++ What_is_class.py
@@ -1,21 +1,32 @@
 """Вложенные классы:
-правила областей видимости LEGB. Присваиваем значение
-тому же самому имени на уровнях вложенных функций."""
+правила областей видимости LEGB. """
+print('-' * 40)
+X = 100
 
-X = 1
+
 def nester():
-    X = 2
-    print(X,'ф-ция nester')
+    X = 30
+    print(f'Ф-ция nester X= {X}')
+
     class C:
-        print(X,'класс С')
+        X = "Атрибут класса С"
+        print(X)
+
         def method_1(self):
-            print(X,'method_1')
+            print(f'Обл.видим в ф-ции method_1 X= {X}')
+            print(f'{self.X} в method_1')
+
         def method_2(self):
             X = 3
-            print(X,'method_2')
+            print(f'Локальная об.видимости method_2 Х={X}')
+            self.X = 333
+            print(f'Экзепляру класса определили атрибут {self.X}')
+
     I = C()
     I.method_1()
     I.method_2()
-print(X,"Глобальный")
+
+
+print(f'Глобальная область ур-нь модуля X= {X}')
 nester()
 print('-' * 40)
