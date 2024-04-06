2b1f2c4b757b942ebcdcf3025e010745c0c1a5da
Perchik <ilya1880@mail.ru>
2024-04-06 18:30:20 +0300

    Итерируемые объекты, определяемые пользователем.

--- What_is_class.py
+++ What_is_class.py
@@ -1,34 +1,23 @@
-"""Индексирование и нарезание:
-             методы
- __getitem__(self, index),
- __setitem__(self, key, value)
- __delitem__(self, key)"""
+"""Итерируемые объекты.
+ Итерируемые объекты, определяемые пользователем.
+            методы
+    __iter__ и __next__"""
+class Squares:
+    def __init__(self, start: int, stop: int):  # Сохранить состояние при создании
+        self.value = start - 1
+        self.stop = stop
+
+    def __iter__(self):  # Получить объект итератора при вызове iter
+        return self
+
+    def __next__(self):  # Возвратить квадрат на каждой итерации
+        if self.value == self.stop:  # Также вызывается встроенной функцией next
+            raise StopIteration
+        self.value += 1
+        return self.value ** 2
 
 
-class C:
-    data = [5, 6, 7, 8, 9]
-
-    def __getitem__(self, index):
-        """Вызывается для индексирования или нарезания"""
-        print('getitem:', [x for x in dir(index) if not x.startswith('__')])
-        return self.data[index]
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-        return self.data
-
-    def __delitem__(self, key):
-        del self.data[key]
-        return self.data
-
-
-
-I = C()
-print(I[2:5:2])
-print(I[slice(2, None)])
-
-I[0]=50
-print(I.data)
-
-del I[3]
-print(I.data)
+X = Squares(5, 9)
+I = iter(X)
+   # ИЛИ
+print(list(X))
