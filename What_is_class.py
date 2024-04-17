2683bcd297f83547f1e7e0c4b9673f1cfe838a6b
Perchik <ilya1880@mail.ru>
2024-04-17 08:08:33 +0300

    Строковое представление: __rерr__ и __str__

--- What_is_class.py
+++ What_is_class.py
@@ -1,38 +1,21 @@
-class C1:
-    """Суперкласс"""
-    x = 5
-    y = 10
+"""Строковое представление: __rерr__ и __str__"""
+class CS:
+    def __init__(self,value):
+        self.value = value
 
+    def __add__(self, other):
+        return self.value + other
 
-class C2:
-    """Суперкласс"""
-    x = 1
-    w = 2
-
-    def __repr__(self):
-        """Перегрузка метода"""
-        return f'Объект класса {__class__.__name__}'
-
-
-class C(C1, C2):
-    """Подкласс"""
-    a = 11
-    y = 12
-
-    def __init__(self, name: str):
-        """Инициализация атрибутов класса"""
-        self.name = name
+class Addrepr(CS):
 
     def __str__(self):
-        """Перегрузка метода"""
-        return f'Объект класса {__class__.__name__}'
+        return f'Объект класса {self.__class__.__name__}'
+
+    def __repr__(self):
+        return f'__repr__ {self.value}'
 
 
-E = C("Илья")  # Экз.класса
-print(E.name)  # Обращение к атрибуту
-E1 = C1()  # Экз.класса
-E2 = C2()  # Экз.класса
-
-print(E)
-print(E1)
-print(E2)
+if __name__ == '__main__':
+    F = Addrepr(4)
+    print(F+5)
+    print(F)
