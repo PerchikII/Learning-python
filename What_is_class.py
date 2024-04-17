1206b751bd4ce3de08fd9766b112d9fc8fcff35f
Perchik <ilya1880@mail.ru>
2024-04-17 09:01:26 +0300

    Метод __call__

--- What_is_class.py
+++ What_is_class.py
@@ -1,21 +1,15 @@
-"""Строковое представление: __rерr__ и __str__"""
-class CS:
-    def __init__(self,value):
-        self.value = value
+"""Метод __call__"""
+class Callee:
+    def __init__(self,data):
+        self.data = data
+    def __call__ (self, other):   # Перехватывает вызовы экземпляра
+        print('Called:', other)
+        return self.data * other
 
-    def __add__(self, other):
-        return self.value + other
-
-class Addrepr(CS):
-
-    def __str__(self):
-        return f'Объект класса {self.__class__.__name__}'
-
-    def __repr__(self):
-        return f'__repr__ {self.value}'
-
+class Callee:
+    def __call__(self, *pargs, **kargs):
+        print('Called:',pargs,kargs)
 
 if __name__ == '__main__':
-    F = Addrepr(4)
-    print(F+5)
-    print(F)
+    C = Callee(5)
+    print(C(3))
