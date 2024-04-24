62d50b1cc94dac039bb9388b784aa3dac5ba1f61
Perchik <ilya1880@mail.ru>
2024-04-24 09:12:47 +0300

    Подмешиваемый класс.Вывод атрибутов с помощью dir().

--- listinherited.py
+++ listinherited.py
@@ -3,19 +3,26 @@ class Listinherited:
 из его классов; в Python З.Х отображается больше имен, чем в Python 2.Х из-за наличия
 подразумеваемого суперкласса object в модели классов нового стиля;
 getattr() извлекает унаследованные имена не в self.__diet__;
-используйте __str__ , а не __ герг__ , иначе произойдет зацикливание при
+используйте __str__ , а не __ rерr__ , иначе произойдет зацикливание при
 вызове связанных методов!"""
-    def attrnames(self):
-        result = ''
-        for attr in dir(self):
-            if attr[:2] == '__' and attr [-2:] == '__':
-                result += f'\t{attr}\n'
-            else:
-                result += f'\t{attr}={getattr(self, attr)}\n'
-        return result
-    def __str__(self):
-        return f'Instance of {self.__class__.__name__}, address {id(self)}:\n{self.attrnames()}'
 
-if __name__ == '__main__' :
+    def __attrnames(self, indent=' ' * 4):
+        result = 'Unders%s\n%s%%s\nOthers%s\n' % ('-' * 77, indent, '-' * 77)
+        unders = []
+        for attr in dir(self):
+            if attr[: 2] == '__' and attr[-2:] == '__':
+                unders.append(attr)
+            else:
+                display = str(getattr(self, attr))[:82 - (len(indent) + len(attr))]
+                result += '%s%s=%s\n' % (indent, attr, display)
+        return result % ','.join(unders)
+    def __str__ (self) :
+        return (f'Instance of {self.__class__.__name__}\n'
+                f'address {id(self)}:\n'
+                f'атрибуты:\n {self.__attrnames()}')
+
+
+if __name__ == '__main__':
     import testmixin
+
     testmixin.tester(Listinherited)
